1759dd2a7d17dfbb3d39f32aca6c16c557acd2c4
iffigues <iffigues@vivaldi.net>
2022-03-31 01:11:27 +0200

    erase

--- nm/ft_nm.c
+++ nm/ft_nm.c
@@ -1,44 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_nm.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bordenoy <bordenoy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 01:06:32 by bordenoy          #+#    #+#             */
+/*   Updated: 2022/03/31 01:10:58 by bordenoy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_nm.h"
 
-void failed(char *a)
+void	failed(char *a)
 {
 	write(1, a, ft_strlen(a));
 	return ;
 }
 
-
-void getstat(t_elf fle)
+void	getstat(t_elf fle)
 {
 	if (!verif(&fle))
 		return (failed("not elf"));
-	if (fle.sys != 3) {
+	if (fle.sys != 3)
+	{
 		if (!endian(&fle))
 			return (failed("not un indien"));
 		if (!systems(&fle))
 			return (failed("not 64_32"));
-		if (fle.sys == 1) {
+		if (fle.sys == 1)
 			ft_32(fle, 0);
-		}
-		if (fle.sys == 2) {
+		if (fle.sys == 2)
 			ft_64(fle, 0);
-		}
-	} else {
-		ar(fle, 0);
 	}
+	else
+		ar(fle, 0);
 }
 
-void	 ft_nm(char *a)
+void	ft_nm(char *a)
 {
 	int	fd;
 	t_elf	fle;
 
-	if ((fd = open(a, O_RDONLY)) < 0)
-		return failed("error on open file\n");
+	fd = open(a, O_RDONLY);
+	if (fd < 0)
+		return (failed("error on open file\n"));
 	if ((fstat(fd, &fle.buf)) < 0)
-		return failed("error with fstats");
+		return (failed("error with fstats"));
 	fle.ptr = mmap(0, fle.buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 	if (fle.ptr == MAP_FAILED)
-		return failed("mmap failed");
+		return (failed("mmap failed"));
 	fle.name = a;
 	fle.len = fle.buf.st_size;
 	getstat(fle);
